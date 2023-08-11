a3e23ec030076c8a9ba8aa9c1e46ba54ce127559
Tatyana277 <tata.nikulina77@gmail.com>
2023-08-11 22:19:49 +0300

    v1

---
+++ js/task-02.js
@@ -0,0 +1,18 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+const ulEl = document.querySelector("#ingredients");
+ulEl.insertAdjacentHTML("afterbegin",
+`<li class="item"></li>`
+
+ const liEl = document.createElement("li");
+  
+liEl.textContent = ingredients(elem);
+});
+);
